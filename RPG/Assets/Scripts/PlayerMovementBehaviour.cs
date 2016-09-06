d7a3b7a5335ae71ea9fcaa755c20bab1fcb28261
Michael Douglas Pacheco <michael.douglas1995@hotmail.com>
2016-09-06 16:54:55 -0300

    Correção do bug de animação quando repete movimento. Código comentado

--- RPG/Assets/Scripts/PlayerMovementBehaviour.cs
+++ RPG/Assets/Scripts/PlayerMovementBehaviour.cs
@@ -14,46 +14,55 @@ public class PlayerMovementBehaviour : MonoBehaviour {
 	public enum PlayerState {
 		STOPPED = 1,
 		WALKING = 2,
-		RUNNING = 3
+		RUNNING = WALKING // Ainda sem animação de correr
 	}
 
 	public KeyCode[] moveKeys = {
 		KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D
 	};
 
-	public PlayerDirection direction;
-	public PlayerState state;
-	public Vector3 destino;
-	public float velocity = 3.0f;
-	public int tileHeight;
-	public int tileWidth;
-	public float delayTime;
-	public Animator animator;
+	public PlayerDirection direction; // Direção para a qual o player está olhando
+	public PlayerState state;		  // Estado atual
+	public Vector3 destiny;			  // Direção para a qual o player deve andar
+	public float velocity = 3.0f;     // Velocidade de animação
+	public float tileSize;            // Largura e altura de um tile em Unity units
+	public float delayTime;			  // O player só se movimenta para uma determinada direção se o keyRecord dessa
+									  // direção for igual ao delayTime
+	public Animator animator;		  // Controlador de animações
 
-	public Dictionary<KeyCode, float> keyRecord;
+	public Dictionary<KeyCode, float> keyRecord; // Armazena tempo em que as teclas estão pressionadas
 
 	// Use this for initialization
 	void Start () {
-		destino = transform.position;
-		tileWidth = 1;
-		tileHeight = 1;
+		destiny = transform.position;
+		tileSize = 1f;
+
+		// Inicia parado virado para cima
 		direction = PlayerDirection.UP;
 		state = PlayerState.STOPPED;
+
 		keyRecord = new Dictionary<KeyCode, float> ();
 		delayTime = 0.15f;
 		animator = GetComponent<Animator> ();
 
+		// Inicia as keyRecords com 0
 		foreach (var key in moveKeys)
 			keyRecord.Add (key, 0f);
 	}
 
+	// Aumenta ou diminui uma keyRecord se a key estiver pressionada
 	void UpdateKeyRecords () {
 		foreach (var key in moveKeys) {
 			if (Input.GetKey (key))
 				keyRecord [key] += Time.deltaTime;
-			else
-				keyRecord [key] -= Time.deltaTime;
+			else {
+				// Esse if é um truque para que não seja preciso esperar o tempo inteiro
+				// para repetir um movimento anterior
+				if (!(IsMoving() && direction == KeyToDirection(key)))
+					keyRecord [key] -= 0.005f;
+			}
 
+			// Mantém a as keysRecords em um intervalo [0, delayTime]
 			if (keyRecord [key] < 0)
 				keyRecord [key] = 0;
 			else if (keyRecord [key] > delayTime)
@@ -70,25 +79,72 @@ public class PlayerMovementBehaviour : MonoBehaviour {
 	}
 
 	void UpdatePlayerAnimationState () {
-		if (TryingToMove ()) {
-			if (IsMoving ())
-				state = Input.GetKey (KeyCode.LeftShift) ? PlayerState.RUNNING : PlayerState.WALKING;
-			else
+		
+		if (IsMoving ())
+			state = Input.GetKey (KeyCode.LeftShift) ? PlayerState.RUNNING : PlayerState.WALKING;
+		else {
+			if (!TryingToMove())
 				state = PlayerState.STOPPED;
-		} else 
-			state = PlayerState.STOPPED;
+		}
 		animator.SetInteger ("PlayerState", (int) state);
 		animator.SetInteger ("PlayerDirection", (int) direction);
+	}
+
+	PlayerDirection KeyToDirection(KeyCode key) {
+		if (key == KeyCode.W)
+			return PlayerDirection.UP;
+		if (key == KeyCode.A)
+			return PlayerDirection.LEFT;
+		if (key == KeyCode.S)
+			return PlayerDirection.DOWN;
+		if (key == KeyCode.D)
+			return PlayerDirection.RIGHT;
+
+		Debug.LogError ("INVALID KEY");
+		return PlayerDirection.UP;
 	}
 
 	void GetUserInput() {
 		if (IsMoving ())
 			return;
 
+		foreach (var key in keyRecord) {
+			// Condição para que possa se mover sem que seja necessário esperar o keyRecord atingir o delayTime
+			if (key.Value == delayTime) {
+				switch (key.Key) {
+				case KeyCode.W:
+					if (direction == KeyToDirection(key.Key) && keyRecord[key.Key] == delayTime)
+						destiny.y += tileSize;
+					else
+						direction = KeyToDirection(key.Key);
+					break;
+				case KeyCode.A:
+					if (direction == KeyToDirection(key.Key) && keyRecord[key.Key] == delayTime)
+						destiny.x -= tileSize;
+					else
+						direction = KeyToDirection(key.Key);
+					break;
+				case KeyCode.S:
+					if (direction == KeyToDirection(key.Key) && keyRecord[key.Key] == delayTime)
+						destiny.y -= tileSize;
+					else
+						direction = KeyToDirection(key.Key);
+					break;
+				case KeyCode.D:
+					if (direction == KeyToDirection(key.Key) && keyRecord[key.Key] == delayTime)
+						destiny.x += tileSize;
+					else
+						direction = KeyToDirection(key.Key);
+					break;
+				}
+				return;
+			}
+		}
+
 		if (Input.GetKey(KeyCode.W)) {
 			if (direction == PlayerDirection.UP) {
 				if (keyRecord [KeyCode.W] == delayTime)
-					destino.y += tileHeight;
+					destiny.y += tileSize;
 			} else
 				direction = PlayerDirection.UP;
 		}
@@ -96,7 +152,7 @@ public class PlayerMovementBehaviour : MonoBehaviour {
 		else if (Input.GetKey(KeyCode.A)) {
 			if (direction == PlayerDirection.LEFT) {
 				if (keyRecord [KeyCode.A] == delayTime)
-					destino.x -= tileWidth;
+					destiny.x -= tileSize;
 			}
 			else
 				direction = PlayerDirection.LEFT;
@@ -105,7 +161,7 @@ public class PlayerMovementBehaviour : MonoBehaviour {
 		else if (Input.GetKey(KeyCode.S)) {
 			if (direction == PlayerDirection.DOWN) {
 				if (keyRecord [KeyCode.S] == delayTime)
-					destino.y -= tileHeight;
+					destiny.y -= tileSize;
 			}
 			else
 				direction = PlayerDirection.DOWN;
@@ -114,7 +170,7 @@ public class PlayerMovementBehaviour : MonoBehaviour {
 		else if (Input.GetKey(KeyCode.D)) {
 			if (direction == PlayerDirection.RIGHT) {
 				if (keyRecord [KeyCode.D] == delayTime)
-					destino.x += tileWidth;
+					destiny.x += tileSize;
 			}
 			else
 				direction = PlayerDirection.RIGHT;
@@ -122,23 +178,23 @@ public class PlayerMovementBehaviour : MonoBehaviour {
 	}
 
 	void Move() {
-		Vector3 paraPercorrer = destino - transform.position;
-		Vector3 passo = paraPercorrer.normalized * velocity * Time.deltaTime;
-		if (paraPercorrer.magnitude <= passo.magnitude) {
-			transform.position = destino;
+		Vector3 toAdd = destiny - transform.position;
+		Vector3 step = toAdd.normalized * velocity * Time.deltaTime;
+		if (toAdd.magnitude <= step.magnitude) {
+			transform.position = destiny;
 		}
 		else {
-			transform.position += passo;
+			transform.position += step;
 		}
 
-		if (passo.x < 0 && Mathf.Abs(passo.x) > Mathf.Abs(passo.y)) direction = PlayerDirection.LEFT;
-		if (passo.x > 0 && Mathf.Abs(passo.x) > Mathf.Abs(passo.y)) direction = PlayerDirection.RIGHT;
-		if (passo.y < 0 && Mathf.Abs(passo.x) <= Mathf.Abs(passo.y)) direction = PlayerDirection.DOWN;
-		if (passo.y > 0 && Mathf.Abs(passo.x) <= Mathf.Abs(passo.y)) direction = PlayerDirection.UP;
+		if (step.x < 0 && Mathf.Abs(step.x) > Mathf.Abs(step.y)) direction = PlayerDirection.LEFT;
+		if (step.x > 0 && Mathf.Abs(step.x) > Mathf.Abs(step.y)) direction = PlayerDirection.RIGHT;
+		if (step.y < 0 && Mathf.Abs(step.x) <= Mathf.Abs(step.y)) direction = PlayerDirection.DOWN;
+		if (step.y > 0 && Mathf.Abs(step.x) <= Mathf.Abs(step.y)) direction = PlayerDirection.UP;
 	}
 
 	bool IsMoving() {
-		return transform.position != destino;
+		return transform.position != destiny;
 	}
 
 	bool TryingToMove () {
@@ -146,5 +202,18 @@ public class PlayerMovementBehaviour : MonoBehaviour {
 			if (Input.GetKey (key))
 				return true;
 		return false;
+	}
+
+
+	void OnTriggerEnter2D (Collider2D col) {
+
+		switch (direction) {
+		case PlayerDirection.UP:
+			transform.Translate (Vector3.down);
+			destiny = transform.position;
+			break;
+		}
+
+		Debug.Log ("TRIGGER");
 	}
 }
---
+++ RPG/Assets/Scripts/SquareBehaviour.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MotionDebug {
+
+	public enum Direction {
+		UP    = 1,
+		RIGHT = 2,
+		DOWN  = 3,
+		LEFT  = 4
+	}
+
+	public enum AnimationState {
+		WAITING = 1,
+		STOPPED = 1,
+		WALKING = 2,
+		RUNNING = 3
+	}
+
+	public class SquareBehaviour : MonoBehaviour {
+
+		private Direction direction;
+		private Vector3 destiny;
+		private bool doingSomething;
+		private float waitingTime;
+		private AnimationState animState;
+		private int tileSize;
+		private float velocity;
+		private float delayTime;
+		private Animator animator;
+
+		private KeyCode[] moveKeys = {
+			KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D
+		};
+
+		private Dictionary<KeyCode, float> keyRecord;
+
+		void Start () {
+			direction = Direction.UP;
+			destiny = transform.position;
+			doingSomething = false;
+			waitingTime = 0;
+			animState = AnimationState.STOPPED;
+			tileSize = 1;
+			velocity = 3f;
+			delayTime = 0.15f;
+			animator = GetComponent<Animator> ();
+
+			keyRecord = new Dictionary<KeyCode, float> ();
+
+			foreach (var key in moveKeys)
+				keyRecord.Add (key, 0f);
+		}
+
+		void Update () {
+			UpdateKeyRecords ();
+			GetUserInput ();
+			Move ();
+			UpdateWaitingTime ();
+
+			animator.SetInteger ("PlayerState", (int) animState);
+			animator.SetInteger ("PlayerDirection", (int) direction);
+		}
+
+		void GetUserInput() {
+			foreach (var key in keyRecord) {
+				if (key.Value == delayTime) {
+					MoveTo (KeyToDirection (key.Key), Input.GetKeyDown(KeyCode.LeftShift));
+					break;
+				}
+			}
+		}
+
+		Direction KeyToDirection(KeyCode key) {
+			if (key == KeyCode.W)
+				return Direction.UP;
+			if (key == KeyCode.A)
+				return Direction.LEFT;
+			if (key == KeyCode.S)
+				return Direction.DOWN;
+			if (key == KeyCode.D)
+				return Direction.RIGHT;
+
+			Debug.LogError ("INVALID KEY");
+			return Direction.UP;
+		}
+
+		void UpdateKeyRecords () {
+			foreach (var key in moveKeys) {
+				if (Input.GetKey (key))
+					keyRecord [key] += Time.deltaTime;
+				else
+					keyRecord [key] -= 0.001f;
+
+				if (keyRecord [key] < 0)
+					keyRecord [key] = 0;
+				else if (keyRecord [key] > delayTime)
+					keyRecord [key] = delayTime;
+			}
+		}
+
+		void UpdateWaitingTime () {
+			if (waitingTime > 0) {
+				waitingTime -= Time.deltaTime;
+				animState = AnimationState.WAITING;
+
+				if (animState <= 0) {
+					animState = 0;
+					animState = AnimationState.STOPPED;
+				}
+			}
+		}
+
+		public void LookAt (Direction direction) {
+			if (animState == AnimationState.STOPPED)
+				this.direction = direction;
+		}
+
+		public bool CanMoveTo (Direction direction) {
+			return true;
+		}
+
+
+		public void Wait(float time) {
+			if (time <= 0)
+				return;
+			
+			if (animState == AnimationState.STOPPED) {
+				waitingTime = time;
+				animState = AnimationState.WAITING;
+				doingSomething = true;
+			}
+		}
+
+		public void MoveTo (Direction direction, bool run) {
+			if (animState == AnimationState.STOPPED && CanMoveTo(direction)) {
+				doingSomething = true;
+				animState = run ? AnimationState.RUNNING : AnimationState.WALKING;
+
+				destiny = transform.position;
+
+				switch (direction) {
+
+				case Direction.UP:
+					destiny.y += tileSize;
+					break;
+				case Direction.DOWN:
+					destiny.y -= tileSize;
+					break;
+				case Direction.LEFT:
+					destiny.x -= tileSize;
+					break;
+				case Direction.RIGHT:
+					destiny.x += tileSize;
+					break;
+
+				}
+
+				Move ();
+			}
+		}
+
+		public bool IsDoingSomething () {
+			if (transform.position != destiny || animState == AnimationState.WAITING)
+				return true;
+			
+			return false;
+		}
+
+		private void Move () {
+			Vector3 paraPercorrer = destiny - transform.position;
+			Vector3 passo = paraPercorrer.normalized * velocity * Time.deltaTime;
+
+			if (paraPercorrer.magnitude <= passo.magnitude)
+				transform.position = destiny;
+			else 
+				transform.position += passo;
+
+			if (passo.x < 0 && Mathf.Abs(passo.x) > Mathf.Abs(passo.y)) direction = Direction.LEFT;
+			if (passo.x > 0 && Mathf.Abs(passo.x) > Mathf.Abs(passo.y)) direction = Direction.RIGHT;
+			if (passo.y < 0 && Mathf.Abs(passo.x) <= Mathf.Abs(passo.y)) direction = Direction.DOWN;
+			if (passo.y > 0 && Mathf.Abs(passo.x) <= Mathf.Abs(passo.y)) direction = Direction.UP;
+
+			doingSomething = transform.position == destiny ? false : true;
+			animState = doingSomething ? animState : AnimationState.STOPPED;
+		}
+	}
+
+}
